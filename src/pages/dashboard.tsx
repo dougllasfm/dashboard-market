780290150c2c018bdd288b7022360907a8aa8a76
dougllasfm <douglasmunaro@gmail.com>
2022-03-03 01:49:29 -0300

    page order criado, chart.js inserido no dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,71 @@
-import type { GetServerSideProps, NextPage } from "next";
+import React from "react";
+import type { GetServerSideProps } from "next";
 import { parseCookies } from "nookies";
 import Layout from "../components/Layout";
+import { Bar } from "react-chartjs-2";
 
-const Dashboard: NextPage = () => {
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+  
+};
+
+const labels = [
+  "Segunda",
+  "Terça",
+  "Quarta",
+  "Quinta",
+  "Sexta",
+  "Sábado",
+  "Domingo",
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Vendas",
+      data: labels.map(() => Math.random() * (120 - 0) + 0),
+      backgroundColor: "green",
+    },
+  ],
+};
+const Dashboard = () => {
   return (
     <Layout>
-      <h1>Ínicio</h1>
+      <Bar options={options} data={data} />
     </Layout>
   );
 };
+
+export default Dashboard;
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { "market.token": token } = parseCookies(ctx);
@@ -25,5 +82,3 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     props: {},
   };
 };
-
-export default Dashboard;
